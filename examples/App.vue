761a65689448c5f16c74ef01f7561dfb056cb7f3
laowan <11513313+laowans@user.noreply.gitee.com>
2023-08-26 00:01:57 +0800

    完成项目结构

---
+++ examples/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<div id="app">app</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'App',
+};
+</script>
+
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+	transpileDependencies: true,
+	productionSourceMap: false,
+	lintOnSave: false,
+	pages: {
+		index: {
+			// 修改项目入口文件，用于测试
+			entry: './examples/main.ts',
+			template: './examples/index.html',
+		},
+	},
+});
