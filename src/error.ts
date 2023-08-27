ea846de346f7fca92d11ab19a9a28ea199b35a18
laowan <11513313+laowans@user.noreply.gitee.com>
2023-08-27 21:15:52 +0800

    扩展错误类属性

--- src/error.ts
+++ src/error.ts
@@ -10,11 +10,17 @@ export class ImageTypeError extends Error implements BaseError {
 		const msg = `Unsupported image type: ${target}, please choose the following format: ${accept.join(', ')}`;
 		super(msg);
 		this.name = 'ImageTypeError';
+		this.target = target;
+		this.accept = accept;
 	}
 
 	get code() {
 		return 0;
 	}
+
+	readonly target;
+
+	readonly accept;
 }
 
 /**
@@ -25,11 +31,16 @@ export class TmageFileTooLarge extends Error implements BaseError {
 		const msg = `Image too large: ${actual}KB, please choose a size smaller than ${target}KB`;
 		super(msg);
 		this.name = 'TmageTooLarge';
+		this.actual = actual;
+		this.target = target;
 	}
 
 	get code() {
 		return 1;
 	}
+
+	readonly actual;
+	readonly target;
 }
 
 /**
@@ -40,9 +51,17 @@ export class ImageDimensionsTooSmall extends Error implements BaseError {
 		const msg = `Image too small: ${w}x${h}, please choose a size larger than ${target}x${target}`;
 		super(msg);
 		this.name = 'ImageDimensionsTooSmall';
+
+		this.w = w;
+		this.h = h;
+		this.target = target;
 	}
 
 	get code() {
 		return 2;
 	}
+
+	readonly w;
+	readonly h;
+	readonly target;
 }
